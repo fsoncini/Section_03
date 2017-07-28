30af8703d2861ea4fb62a2ea12f3105e223bb768
Federico Soncini <fsonc002@gold.ac.uk>
2017-07-28 17:03:37 +0200

    created FOmOpenRequest class

--- BuildingEscape_NEW/Source/BuildingEscape_NEW/OpenDoor.cpp
+++ BuildingEscape_NEW/Source/BuildingEscape_NEW/OpenDoor.cpp
@@ -29,9 +29,9 @@ void UOpenDoor::BeginPlay()
 }
 
 void UOpenDoor::OpenDoor()
-{	
-	//Set the door rotation	
-	Owner->SetActorRotation(FRotator(0.0f, OpenAngle, 0.0f));
+{		
+	//Owner->SetActorRotation(FRotator(0.0f, OpenAngle, 0.0f));
+	OnOpenRequest.Broadcast();
 }
 
 
